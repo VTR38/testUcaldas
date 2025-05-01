077ec8c9a9da21f6dc1b2e06972cd20c86dc598e
Victor <victor.reyes44844@ucaldas.edu.co>
2025-04-30 19:31:01 -0500

    First commit with change

---
+++ WebAppVeterinaria/Models/Medico.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAppVeterinaria.Models
+{
+    public class Medico
+    {
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Genero { get; set; }
+    }
+}
---
+++ WebAppVeterinaria/wwwroot/js/componentesMaterialize.js
@@ -0,0 +1,14 @@
+$(function(){
+	// $('.datepicker').pickadate({
+	// 	selectMonths: true, // Creates a dropdown to control month
+	// 	selectYears: 15 // Creates a dropdown of 15 years to control year
+	// });
+
+	$(document).ready(function(){
+		$('.modal').modal();
+	  });
+
+	$(document).ready(function(){
+		$('.collapsible').collapsible();
+	  });
+});
